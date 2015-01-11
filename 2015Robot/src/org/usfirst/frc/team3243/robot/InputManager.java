95778c560f51ac2792cc09825475795263ebe7c2
tarun <tarun.sunkaraneni@gmail.com>
2015-01-10 23:40:12 -0700

    Refactored, added comments. If you have any questions contact Tarun

--- 2015Robot/src/org/usfirst/frc/team3243/robot/InputManager.java
+++ 2015Robot/src/org/usfirst/frc/team3243/robot/InputManager.java
@@ -5,50 +5,66 @@ import edu.wpi.first.wpilibj.*;
 
 public class InputManager extends Robot {
 	
-	static double[] axis = new double [2];
+	static double[] axis = new double [3];//holds the input value from our left/right controllers
 	
-	protected static Joystick ps2controller;
+	protected static Joystick ps2controller;//our controller
 	
-	protected static RobotMap R;
+	protected static int rampUpNum = 3;//used for ramp up
 	
+	//initializes the controller
 	public void init() {
 		
 		ps2controller = new Joystick(1);
-		R = new RobotMap();
+		//rampUpNum = int
 		
 	}
-	
+	/**
+	 * 
+	 * It retrieves the getAxisValue class's values, deadzones, and then it calls the ramp method, which regresses it to be a cubu=ic function rather than a line
+	 * @return
+	 */
 	public static double [] getFinalAxis(){
-		double[] mtrval = new double[2];
-		mtrval = ramp(getAxisValue());
-		return mtrval;
+		return ramp(getAxisValue());
+		//three things happen in this class.
+		//1)you get axis values
+		//2)then you deadzone the values
+		//3) You transform the deadzoned values into a cubic equation
 	}
 	
 	public static double[] getAxisValue(){
 		
 		axis[0] = ps2controller.getRawAxis(2);//y axis 
 		axis[1] = ps2controller.getRawAxis(1);//x axis
-		axis[2] = ps2controller.getRawAxis(3);
-		axis = deadZone(axis);
+		axis[2] = ps2controller.getRawAxis(3);//pivioting
+		axis = deadZone(axis);//transforms the array to deadzone to round values as necessary (ex. -0.03 to 0)
 		return axis;
 		
 		
 	}
 	
+	/**
+	 * transforms the array to deadzone to round values as necessary (ex. 0.02 to 0)
+	 * @param axis
+	 * @return
+	 */
 	public static double[] deadZone(double[] axis){
-		for (byte si = 0; si< axis.length; si++){
-			if ((axis[si] <= 0.05) && (axis[si] >= -0.05)) {
-				axis[si] = 0;
+		for (int i = 0; i< axis.length; i++){
+			if ((axis[i] <= 0.05) && (axis[i] >= -0.05)) {//i will iterate to 0,1,2 since axis array size is 3
+				axis[i] = 0;//if the condition is satisfies, round it to zero
 			}
 		}
 		return axis;
 	}
-	
+	/**
+	 * transforms the values in the array to it's coresponding cubica values, based on formulas
+	 * @param axis
+	 * @return
+	 */
 	public static double[] ramp(double[] axis){
-		for (byte si = 0; si<2; si++){
+		for (int i = 0; i<2; i++){
 			
-			axis[si] = (((.667)*(Math.pow(axis[si], 3)))+((.333)*(axis[si])));
-			
+			axis[i] = (((.667)*(Math.pow(axis[i], 3)))+((.333)*(axis[i])));
+			//if you want to graph it, it is y=0.66x^3+0.33x
 		}
 		return (axis);
 	}
