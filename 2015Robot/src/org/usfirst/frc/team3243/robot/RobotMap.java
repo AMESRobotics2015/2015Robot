849a2b58c6b8ac768e494897f55ee74e3e928ca8
AMESRobotics2015 <ames.amperes@gmail.com>
2015-01-12 16:14:23 -0800

    finished RobotMap

--- 2015Robot/src/org/usfirst/frc/team3243/robot/RobotMap.java
+++ 2015Robot/src/org/usfirst/frc/team3243/robot/RobotMap.java
@@ -1,5 +1,10 @@
 package org.usfirst.frc.team3243.robot;
-
+import edu.wpi.first.wpilibj.*;
 public class RobotMap {
+	
+protected static final int A = 0;
+protected static final int B = 1;
+protected static final int C = 2;
+protected static final int D = 3;
 
 }
